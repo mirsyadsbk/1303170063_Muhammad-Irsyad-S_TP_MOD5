f0f509e1c44914fd6c5feda2149ebe40e25172f0
mirsyadsbk <55907438+mirsyadsbk@users.noreply.github.com>
2019-09-29 08:34:16 +0700

    Add files via upload

---
+++ todos.js
@@ -0,0 +1,46 @@
+$('li').click(function() {
+  if ($(this).css('color') === 'rgb(128, 128, 128)') {
+    $(this).css({
+      color: 'black',
+      textDecoration: 'none'
+    });
+  } else {
+    $(this).css({
+      color: 'gray',
+      textDecoration: 'line-through'
+    });
+  }
+});
+
+$('#semangat').append(
+  "<audio controls autoplay hidden> <source src='../css/semangat.mp3' type='audio/mpeg' /></audio>"
+  );
+
+// lengkapi jquery untuk menlist hasil dari todolist yang di inputkan
+$('ul').on('click', 'li', function() {
+  $(this).css({
+    color: 'gray',
+    textDecoration: 'line-through'
+  });
+});
+
+// buatlah jquery untuk menekan tombol akan menjadikan data sebagai list
+$('#plus').on('click', function(){
+  if ($('#todo-input').val() != null && $('#todo-input').val() != '') {
+    let myInput = $('#todo-input').val();
+    let isiHtml = `
+    <li>
+    <span><i class="fa fa-trash"></i></span> ${myInput}
+    </li>
+    `;
+    $('#todos').append(isiHtml);
+    $('#todo-input').val('');
+  }
+})
+
+
+// load();
+// function load() {
+//   var mydata = JSON.parse(JSON.stringify(data))
+//   console.log(mydata.home[0].nama);
+// }
